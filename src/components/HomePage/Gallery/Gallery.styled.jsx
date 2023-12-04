5d58cdac4c1549b6092f6c5e6c1c2d7f22f478c9
Yevhenii-Lukianenko <zhenyalukyanenko17@gmail.com>
2023-12-04 21:44:42 +0200

    Пофіксив масштаби галереї

--- src/components/HomePage/Gallery/Gallery.styled.jsx
+++ src/components/HomePage/Gallery/Gallery.styled.jsx
@@ -1,11 +1,15 @@
 import styled from '@emotion/styled';
 
+export const ImageWrapper = styled.div`
+  padding: 0 30px;
+`;
+
 export const Image = styled.img`
   height: 300px;
   @media screen and (min-width: 768px) {
-    height: 400px;
+    height: 500px;
   }
   @media screen and (min-width: 1200px) {
-    height: 600px;
+    height: 800px;
   }
 `;
